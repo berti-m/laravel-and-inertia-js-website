5f4113bce48159cba17459d6177bd5f62b544abe
berti-m <mberteti@gmail.com>
2022-07-07 19:51:05 +0200

    Added active link displaying

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,14 +1,23 @@
 <template>
 	<nav>
-		<ul class="underline text-blue-600">
-			<li><Link href="/">Home</Link></li>
-			<li><Link href="/users">Users</Link></li>
-			<li><Link href="/settings">Settings</Link></li>
-			<li><Link href="/logout" method="post" as="button" :data="{ foo: 'bar' }">Log Out</Link></li>
+		<ul>
+			<NavLink href="/" :active="$page.component == 'Home'">
+				Home
+			</NavLink>
+			<NavLink href="/users" :active="$page.component == 'Users'">
+				Users
+			</NavLink>
+			<NavLink href="/settings" :active="$page.component == 'Settings'">
+				Settings
+			</NavLink>
+			<li>
+				<Link href="/logout" method="post" as="button" :data="{ foo: 'bar' }" class="text-red-500 hover:underline">Log Out</Link>
+			</li>
 		</ul>
 	</nav>
 </template>
 
 <script setup>
 	import { Link } from "@inertiajs/inertia-vue3";
+	import NavLink from "./NavLink";
 </script>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,22 @@
+<template>
+	<li>
+		<Link 
+		:href="href"
+		class="text-blue-600 hover:underline"
+		:class="{'underline font-bold': active}"
+		>
+			<slot />
+		</Link>
+	</li>
+</template>
+
+<script setup>
+	import { Link } from "@inertiajs/inertia-vue3";
+	defineProps({
+		href: String,
+		active: {
+			type: Boolean,
+			value: false
+		}
+	})
+</script>
