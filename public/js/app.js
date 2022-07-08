f88ea79b773aebc32cd172fa0497c7495ed334a3
berti-m <mberteti@gmail.com>
2022-07-08 10:23:56 +0200

    Globally shared data

--- public/js/app.js
+++ public/js/app.js
@@ -21774,14 +21774,23 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Shared_Nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Shared/Nav */ "./resources/js/Shared/Nav.vue");
+/* harmony import */ var _inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @inertiajs/inertia-vue3 */ "./node_modules/@inertiajs/inertia-vue3/dist/index.js");
+
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   __name: 'Layout',
   setup: function setup(__props, _ref) {
     var expose = _ref.expose;
     expose();
+
+    function username() {
+      return (0,_inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_1__.usePage)().props.value.auth.user.username;
+    }
+
     var __returned__ = {
-      Nav: _Shared_Nav__WEBPACK_IMPORTED_MODULE_0__["default"]
+      username: username,
+      Nav: _Shared_Nav__WEBPACK_IMPORTED_MODULE_0__["default"],
+      usePage: _inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_1__.usePage
     };
     Object.defineProperty(__returned__, '__isScriptSetup', {
       enumerable: false,
@@ -22002,16 +22011,25 @@ __webpack_require__.r(__webpack_exports__);
 var _hoisted_1 = {
   "class": "flex p-3 rounded-lg bg-gray-200 justify-between text-lg border border-black mb-4"
 };
+var _hoisted_2 = {
+  "class": "flex items-center space-x-4"
+};
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h1", null, "My App", -1
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, " My App ", -1
 /* HOISTED */
 );
 
-var _hoisted_3 = {
+var _hoisted_4 = {
+  key: 0,
+  "class": "text-sm"
+};
+var _hoisted_5 = {
   "class": "p-3"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", _hoisted_1, [_hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["Nav"])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.renderSlot)(_ctx.$slots, "default")])], 64
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", _hoisted_2, [_hoisted_3, $setup.username() != null ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("span", _hoisted_4, " Welcome back, " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($setup.username()) + "! ", 1
+  /* TEXT */
+  )) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)($setup["Nav"])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("section", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.renderSlot)(_ctx.$slots, "default")])], 64
   /* STABLE_FRAGMENT */
   );
 }
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,6 +1,13 @@
 <template>
 	<section class="flex p-3 rounded-lg bg-gray-200 justify-between text-lg border border-black mb-4">
-		<h1>My App</h1>
+		<span class="flex items-center space-x-4">
+			<span>
+				My App
+			</span>
+			<span class="text-sm" v-if="username() != null">
+				Welcome back, {{ username() }}!
+			</span>
+		</span>
 		<Nav/>
 	</section>
 	<section class="p-3">
@@ -11,4 +18,9 @@
 
 <script setup>
 	import Nav from "../Shared/Nav"
+	import { usePage } from '@inertiajs/inertia-vue3'
+
+	function username(){
+		return usePage().props.value.auth.user.username;
+	}
 </script>
